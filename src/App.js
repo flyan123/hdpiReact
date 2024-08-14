b0befcb2a6eca7145f5c30ddf2ecd3cfdde0abda
flyan123 <2495365901@qq.com>
2024-08-14 23:59:01 +0800

    add 0814

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import 'antd/dist/reset.css'
-import { Button } from 'antd'
-import { Rate } from "antd";
-// import 'antd/dist/antd.css'
 
 const App=() =>{
 return (
 <div>
-    1122
-    <Button>12</Button>
-    <Button type="primary">Primary Button</Button>
-    <Rate allowHalf defaultValue={2.5} />
-    
+   
 </div>
 )
 }
