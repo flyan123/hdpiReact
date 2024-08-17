c91c85e8d57e15342ec41a8b52f69afd06604e32
flyan123 <2495365901@qq.com>
2024-08-17 23:52:11 +0800

    add 0817

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// 配置路由
-// import AppRouter from './router';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-//   <AppRouter/>
-  <App/>
+   <App/>
 ); 
