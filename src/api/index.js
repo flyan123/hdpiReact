11fb40b90373141ede8c9d734469e8f7ce429105
flyan123 <2495365901@qq.com>
2024-09-09 00:00:07 +0800

    add 0908

--- src/api/index.js
+++ src/api/index.js
@@ -4,5 +4,9 @@ import md5 from "md5";
 export const $login=async (params)=>{
     params.loginPwd=md5(md5(params.loginPwd).split('').reverse().join(''))
     let {data} = await axios.get('Admin/Login',{params})
+    if(data.success){
+        sessionStorage.setItem('token',data.token)
+
+    }
     return data
 }
