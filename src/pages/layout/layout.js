618bcec8f4fde227e6bfa443f1bd9afd858b33e5
flyan123 <2495365901@qq.com>
2024-09-16 23:49:52 +0800

    add 0916

--- src/pages/layout/layout.js
+++ src/pages/layout/layout.js
@@ -1,19 +1,61 @@
 import React, { useState } from "react";
+// 图标
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
 } from "@ant-design/icons";
-import { Button, Layout, Menu, theme } from "antd";
+import { Button, Layout, Menu } from "antd";
 import "./layout.scss";
-
 const { Header, Sider, Content } = Layout;
 const layout = () => {
+  // 顶部列表
+  const items = [
+    {
+      label: "首页",
+      key: "home",
+      icon: <MailOutlined />,
+    },
+    {
+      label: "通知",
+      key: "mail",
+      icon: <AppstoreOutlined />,
+      disabled: true,
+    },
+    {
+      label: "邮件",
+      key: "noti",
+      icon: <AppstoreOutlined />,
+      disabled: true,
+    },
+    {
+      label: "个人中心",
+      key: "SubMenu",
+      icon: <SettingOutlined />,
+      children: [
+        {
+          key: "1",
+          label: "个人信息",
+        },
+        {
+          key: "2",
+          label: "修改密码",
+        },
+        {
+          key: "3",
+          label: "退出登录",
+        },
+      ],
+    },
+  ];
+  const [current, setCurrent] = useState("mail");
   // 侧边栏折叠状态
   const [collapsed, setCollapsed] = useState(false);
-
   return (
     <Layout className="layout">
       <Sider trigger={null} collapsible collapsed={collapsed}>
@@ -44,22 +86,24 @@ const layout = () => {
         />
       </Sider>
       <Layout>
-        <Header
+        <Header 
+          className="header"
           style={{
             padding: 0,
-            background: "rgba(0,21,41,0.1)",
+            background: "#ffffff",
           }}
         >
           <Button
-            type="text"
             icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
             onClick={() => setCollapsed(!collapsed)}
             style={{
               fontSize: "16px",
               width: 64,
               height: 64,
+              border:'transparent'
             }}
           />
+          <Menu className="headermenu" selectedKeys={[current]} mode="horizontal" items={items} />;
         </Header>
         <Content>Content</Content>
       </Layout>
