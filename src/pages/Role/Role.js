5eacb9519c6948db38a30e507cc0d48fa15aef94
flyan123 <2495365901@qq.com>
2024-10-20 23:51:13 +0800

    add 1020

--- src/pages/Role/Role.js
+++ src/pages/Role/Role.js
@@ -1,49 +1,52 @@
 import React from "react";
-import { Table ,useEffect,useState} from "antd";
+import { Button, Table ,useEffect,useState} from "antd";
 import { $list } from "../../api/RoleApi";
 import { useFetcher } from "react-router-dom";
 
 export default function Role() {
-  let [roleList,setRoleList]=useState([])
+  // let [roleList,setRoleList]=useState([])
 
-  useEffect(()=>{
-    $list().then()
-  },[])
+  // useEffect(()=>{
+  //   $list().then(data=>{
+  //     data=data.map(r=>{
+  //       return {
+  //         ...r,
+  //         key:r.roleId
+  //       }
+  //     })
+  //     setRoleList(data)
+  //   })
+  // },[])
   const dataSource = [
     {
       key: "1",
-      name: "胡彦斌",
-      age: 32,
-      address: "西湖区湖底公园1号",
+      roleId: "胡彦斌",
+      roleName: "西湖区湖底公园1号",
     },
     {
       key: "2",
-      name: "胡彦祖",
-      age: 42,
-      address: "西湖区湖底公园1号",
+      roleId: "胡彦祖",
+      roleName: "西湖区湖底公园1号",
     },
   ];
 
   const columns = [
     {
-      title: "姓名",
-      dataIndex: "name",
-      key: "name",
+      title: "角色编号",
+      dataIndex: "roleId",
     },
     {
-      title: "年龄",
-      dataIndex: "age",
-      key: "age",
-    },
-    {
-      title: "住址",
-      dataIndex: "address",
-      key: "address",
+      title: "角色名称",
+      dataIndex: "roleName",
     },
   ];
   return (
     <>
+    <div>
+      <Button className="search">添加</Button>
+    </div>
       <Table size="small" dataSource={dataSource} columns={columns} />;
+      {/* <Table size="small" dataSource={roleList} columns={columns} />; */}
     </>
   );
 }
