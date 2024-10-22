b9d4694d29be9051c6b46ce5e9e7ce2cb7328ecb
flyan123 <2495365901@qq.com>
2024-10-22 23:53:57 +0800

    add 1022

--- src/pages/Role/Role.js
+++ src/pages/Role/Role.js
@@ -1,11 +1,22 @@
-import React,{useEffect,useState} from "react";
-import { Button, Table ,Drawer } from "antd";
+import React, { useEffect, useState } from "react";
+import { Button, Table, Drawer, Checkbox, Form, Input } from "antd";
 import { $list } from "../../api/RoleApi";
 import { useFetcher } from "react-router-dom";
 
 export default function Role() {
+  // 表单
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
   // 是否打开抽屉
   const [open, setOpen] = useState(false);
+  // 关闭抽屉的方法
+  const onClose = () => {
+    setOpen(false);
+  };
   // 角色列表数据
   // let [roleList,setRoleList]=useState([])
   // useEffect(()=>{
@@ -44,15 +55,79 @@ export default function Role() {
   ];
   return (
     <>
-    <div>
-      <Button className="search">添加</Button>
-    </div>
+      <div>
+        <Button
+          className="search"
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          添加
+        </Button>
+      </div>
       <Table size="small" dataSource={dataSource} columns={columns} />;
       {/* <Table size="small" dataSource={roleList} columns={columns} />; */}
-      <Drawer title="Basic Drawer" onClose={onClose} open={open}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      <Drawer
+        width={450}
+        title="添加角色"
+        placement="right"
+        onClose={onClose}
+        open={open}
+      >
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="角色编号"
+            name="roleId"
+            rules={[
+              {
+                required: true,
+                message: "Please input your roleId!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="角色名称"
+            name="roleName"
+            rules={[
+              {
+                required: true,
+                message: "Please input your roleName!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary"  htmlType="submit">
+              添加
+            </Button>
+          </Form.Item>
+        </Form>
       </Drawer>
     </>
   );
